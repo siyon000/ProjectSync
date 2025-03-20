54ed334199ab8e01031b4d8b45e15e12aae8c4d4
siyon000 <siyonrai2@gmail.com>
2025-03-20 20:27:18 +0545

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import ProjectListPage from './pages/ProjectListPage';
 import ProjectSubmissionPage from './pages/ProjectSubmissionPage';
@@ -7,26 +7,21 @@ import { AuthProvider } from './context/AuthContext';
 import { ProjectProvider } from './context/ProjectContext';
 import './App.css';
 
-// Fix: Redirect from root (/) or "/ProjectSync/" to "/#/login"
-function RedirectToLogin() {
-  const location = useLocation();
-  if (location.pathname === "/") {
-    return <Navigate to="/login" replace />;
-  }
-  return null;
-}
 
 function App() {
   return (
     <AuthProvider>
       <ProjectProvider>
         <Router>
-          <RedirectToLogin />
           <Routes>
+            {/* <Route path="/login" element={<LoginPage />} />
+            <Route path="/projects" element={<ProjectListPage />} />
+            <Route path="/submit-project" element={<ProjectSubmissionPage />} />
+            <Route path="/" element={<Navigate to="/login" />} /> */}
+            <Route path="/" element={<LoginPage />} /> {/* Default page */}
             <Route path="/login" element={<LoginPage />} />
             <Route path="/projects" element={<ProjectListPage />} />
             <Route path="/submit-project" element={<ProjectSubmissionPage />} />
-            <Route path="*" element={<Navigate to="/login" />} /> {/* Catch-all redirect */}
           </Routes>
         </Router>
       </ProjectProvider>
