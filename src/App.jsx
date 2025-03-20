3af04053926b960b93bcafd2bbb621c84fbbb9d1
siyon000 <siyonrai2@gmail.com>
2025-03-20 20:10:39 +0545

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,11 @@ import { AuthProvider } from './context/AuthContext';
 import { ProjectProvider } from './context/ProjectContext';
 import './App.css';
 
-
 function App() {
   return (
     <AuthProvider>
       <ProjectProvider>
-        <Router>
+        <Router basename="/ProjectSync">
           <Routes>
             <Route path="/login" element={<LoginPage />} />
             <Route path="/projects" element={<ProjectListPage />} />
